5547f63453be2d1d405bf3c1c38f0f5752916a65
Gisela-Gentile <104107443+Gisela-Gentile@users.noreply.github.com>
2022-04-28 19:56:00 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let precioProducto:number = 450.5;
+let porcentajeDescuento:number = 0.1;
+let descueto:number = precioProducto*porcentajeDescuento
+let precioFinal: number=precioProducto-descueto
+console.log("El precio final es:",precioFinal);
+
+
